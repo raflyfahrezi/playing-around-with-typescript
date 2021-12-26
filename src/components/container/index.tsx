cee0da8357b3e4e2cf0093fa821f2744329397d7
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-26 21:38:54 +0700

    edit container

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 }
 
 const Container = ({ children }: ContainerProps) => {
-    return <div className='w-full max-w-2xl mx-auto'>{children}</div>
+    return <div className='w-full max-w-5xl mx-auto'>{children}</div>
 }
 
 export default Container
