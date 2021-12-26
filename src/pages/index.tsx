c8df2e4db73db17521585c8c23e176c50ec3b160
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-26 21:22:31 +0700

    implement pageWrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import type { NextPage } from 'next'
 
 import { HomeModule } from '@/modules'
+import { PageWrapper } from '@/layout'
 
 const index: NextPage = () => {
-    return <HomeModule />
+    return (
+        <PageWrapper>
+            <HomeModule />
+        </PageWrapper>
+    )
 }
 
 export default index
