865442c29b330b55c770e80cd0325589ee1c1f35
Farhan Rafly Fahrezi S <raflyfahr15@gmail.com>
2021-12-26 21:04:05 +0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type { NextPage } from 'next'
+
+const index: NextPage = () => {
+    return (
+        <div className='text-center my-20'>
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+export default index
