77d54f154815c4c87c18612f3b794a352dff64ad
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-26 22:07:36 +0700

    fetching data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
-import type { NextPage } from 'next'
+import axios from 'axios'
+import { GetStaticProps } from 'next'
 
 import { HomeModule } from '@/modules'
 import { PageWrapper } from '@/layout'
 
-const index: NextPage = () => {
+import type { ContactModel } from '@/models'
+
+type indexProps = {
+    contactList: ContactModel[]
+}
+
+const index = ({ contactList }: indexProps) => {
     return (
         <PageWrapper>
-            <HomeModule />
+            <HomeModule contactList={contactList} />
         </PageWrapper>
     )
 }
 
+const getStaticProps: GetStaticProps = async () => {
+    const contactListResponse = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    )
+
+    const contactList = await contactListResponse.data
+
+    return {
+        props: {
+            contactList,
+        },
+    }
+}
+
 export default index
+export { getStaticProps }
